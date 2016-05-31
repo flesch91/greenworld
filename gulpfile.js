7a6ede07ac1eaf141fa19b0ea565499c74b5ea31
tararas <tarasdidg@gmail.com>
2016-05-31 15:19:28 +0300

    chenge file+add project

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/**
+ * Created by haluniki on 31.05.16.
+ */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('source/sass/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('source/assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('source/sass/**/*.scss', ['sass']);
+});
---
+++ source/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by haluniki on 31.05.16.
+ */
