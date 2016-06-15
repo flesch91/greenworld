b92075a4bff6d0992ef00e58de1870518c1b7d44
tararas <tarasdidg@gmail.com>
2016-06-16 01:59:09 +0300

    navigation registration

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,26 @@
 /**
  * Created by haluniki on 31.05.16.
  */
+var text = 'Hello World!' + 1;
+
+var one = 1;
+var two = 2;
+
+var sum = one + two;
+
+var strNumb = 2;
+var plus = '+';
+var equally = '=';
+
+
+var formula = (2 + Math.sqrt(4))/(2 * 2);
+
+
+//-------------------------------------
+
+var n = 2;
+n += 5;
+n *=10;
+
+
+
